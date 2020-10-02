22725d3d608a24cc4fc88abe29b9a0d332e115f6
Rebecca Robbins <rebeccamargaret13@gmail.com>
2020-10-02 04:12:09 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -23,11 +23,8 @@ def get_japanese_emoticon(file_path, eng_emo)
     if value[:english] == eng_emo
       return value[:japanese]
     end
-    end
-    return "Sorry, that emoticon was not found"
-   binding.pry 
-
-  
+  end
+  return "Sorry, that emoticon was not found"
 end
 
 def get_english_meaning
