3d75fc9ab109cf412138a7ecee5d700d2917ca5e
Rebecca Robbins <rebeccamargaret13@gmail.com>
2020-10-02 03:53:29 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,13 +1,27 @@
 # require modules here
+require 'pry'
+require "yaml"
+emoticons = YAML.load_file("lib/emoticons.yml")
 
-def load_library
-  # code goes here
+emoticons 
+
+def load_library(file_path)
+  emoticons = YAML.load_file("lib/emoticons.yml") 
+
+  emoticons.each_with_object({}) do |(key, value), emo_dict|
+    emo_dict[key] = {}
+    emo_dict[key][:english]
+       binding.pry
+  end
+    
 end
 
 def get_japanese_emoticon
   # code goes here
+ 
 end
 
 def get_english_meaning
   # code goes here
+
 end
