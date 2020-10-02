0512a33a4df70fac4162c00b27a60ed92e976b31
Rebecca Robbins <rebeccamargaret13@gmail.com>
2020-10-02 04:09:16 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -29,5 +29,8 @@ def get_japanese_emoticon(file_path, eng_emo)
 end
 
 def get_english_meaning
-  # code goes here
+  emoticons = load_library(file_path)
+  
+  emoticons.each do |key, value|
+  
 end
